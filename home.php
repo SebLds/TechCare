9ec97a21e049d2b635c3f31b297ed16592a511a9
M-Schuch <58336186+M-Schuch@users.noreply.github.com>
2020-04-09 16:16:47 +0200

    Schuch
    Header
    Page de connexion
    Page mot de passe oublié
    
    Début page d'accueil

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Infinite Measures</title>
+    <link rel="stylesheet" href="CSS/rules.css">
+    <link rel="stylesheet" href="CSS/home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<?php include("header.php"); ?>
+
+<body>
+
+  <div class="home section">
+
+    <div class="title">
+      <h1>Effectuez vos tests psychotechniques<span>et consultez vos résultats</span></h1>
+    </div>
+
+    <div class="team">
+      <img id="img" src="images/team.png">
+    </div>
+
+  </div>
+
+  <div class="electronic section">
+
+    <div class="title">
+      <h1>Un boîtier pour réaliser les tests</h1>
+    </div>
+
+  </div>
+
+  <div class="site section">
+
+    <div class="title">
+      <h1>Un site pour consulter ses résultats</h1>
+    </div>
+
+  </div>
+
+  <div class="jsp section">
+
+  </div>
+
+
+
+
+
+</body>
+</html>
