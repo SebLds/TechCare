db11ebb629311418444c04bd3507a7276d50e67c
Sébastien LE DOUSSAL <seb.ledoussal@gmail.com>
2020-05-04 19:39:24 +0200

    Ajout Models User, Register et Dashboard

---
+++ App/Model/Dashboard.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model;
+use PDO;
+use src\Config\ConfigException;
+use src\Model;
+
+class Dashboard
+{
+    function getCountTotalTestsUser($idUser)
+    {
+        try {
+            return $this -> executeRequest('SELECT count(idPretest) as nbTests FROM test WHERE idUtilisateur = ?', $idUser) -> fetch(PDO::FETCH_OBJ);
+        } catch (ConfigException $e) { return false; /*Message d'erreur*/ }
+    } //function getCountTotalTestsUser($id)
+
+
+    function getLastScore($idUser, $value) //Voir avec $value pour faire -1 lorsque l'on veut l'AVANT-dernier test, etc..
+    {
+        try {
+            return $this -> executeRequest("SELECT Score FROM test WHERE (idUtilisateur = ? AND idPretest = MAX(idPretest)-$value", $idUser) -> fetch(PDO::FETCH_OBJ);
+        } catch (ConfigException $e) { return false; /*Message d'erreur*/ }
+    } //function getLastScore($idUser, $value)
+
+
+    function getLastTypeTest($idUser, $value) //Récupérer le type du dernier test seulement
+    {
+        try {
+            return $this -> executeRequest("SELECT Type as typeTest FROM test WHERE (idUtilisateur = ? AND idPretest = MAX(idPretest)-$value", $idUser) -> fetch(PDO::FETCH_OBJ);
+        } catch (ConfigException $e) { return false; /*Message d'erreur*/ }
+    } //function getLastTypeTest($idUser, $value)
+
+
+    function getLastCommentaire($idUser, $value) //Récupérer le commentaire du dernier test
+    {
+        try {
+            return $this -> executeRequest("SELECT commentaire FROM test WHERE (idUtilisateur = ? AND idPretest = MAX(idPretest)-$value", $idUser) -> fetch(PDO::FETCH_OBJ);
+        } catch (ConfigException $e) { return false; /*Message d'erreur*/ }
+    } //function getLastCommentaire($idUser, $value)
+}
--- App/Model/registerModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function checkConnexion($mail, $password) {
-    return executeRequest('SELECT idUtilisateur, "Type" FROM utilisateur WHERE Email = ? AND Motdepasse = ?', $mail, $password);
-} //function checkConnexion($mail, $password)
-
-
