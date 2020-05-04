576de8880d2a3784a02e825342248e816725ac6a
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-04 16:39:50 +0200

    home page

--- App/View/Home/index.php
+++ App/View/Home/index.php
@@ -128,5 +128,6 @@
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../../js/header.js"></script>
 
 </body>
--- App/View/header.php
+++ App/View/header.php
@@ -4,35 +4,30 @@
 </head>
 
 <body>
-<header>
-    <div class="logo">
-      <img id="logo" src="../../images/logo.png" alt="logo">
-    </div>
 
-    <div class="navbar">
-        <li><a href="/homepage" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
-        <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
-        <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
-        <a href="connexion.php" ><button id="button-connexion"><img class="images bis" src="/Web/images/account.png">Connexion</button></a>
-    </div>
+    <header id="header">
+        <div class="logo">
+            <a href="/homepage#banner"><img id="logo" src="../../images/logo.png" alt="logo"></a>
+        </div>
 
-    <div class="burger-button">
-      <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
-      <span></span>
-    </div>
+        <ul class="navbar">
+            <li><a href="/homepage#banner" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
+            <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
+            <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
+            <a href="connexion.php" ><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a>
+        </ul>
 
-    <script
-        src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
-    </script>
+        <div class="burger-button">
+          <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
+          <span></span>
+        </div>
 
-    <script
-        type="text/javascript"
-        src="../../js/burger.js">
-    </script>
+    </header>
 
-</header>
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../../js/burger.js"></script>
+
 </body>
 </html>
 
