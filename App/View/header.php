9d775adca6314f8b072dd0a60d36a08fb681efe9
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-04 17:02:01 +0200

    home page

--- App/View/header.php
+++ App/View/header.php
@@ -5,26 +5,26 @@
 
 <body>
 
-<header>
-    <div class="logo">
-      <img id="logo" src="../../images/logo.png" alt="logo">
-    </div>
+    <header id="header">
+        <div class="logo">
+            <a href="/homepage#banner"><img id="logo" src="../../images/logo.png" alt="logo"></a>
+        </div>
 
-    <ul class="navbar">
-        <li><a href="/homepage" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
-        <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
-        <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
-        <a href="connexion.php" ><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a>
-    </ul>
+        <ul class="navbar">
+            <li><a href="/homepage#banner" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
+            <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
+            <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
+            <a href="connexion.php" ><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a>
+        </ul>
 
-    <div class="burger-button">
-      <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
-      <span></span>
-    </div>
+        <div class="burger-button">
+          <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
+          <span></span>
+        </div>
 
-</header>
+    </header>
 
-<!-- Scripts -->
+    <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../../js/burger.js"></script>
 
