3a187193635e71201fc5e9a6cc3fa720641660be
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-05 19:11:44 +0200

    header responsive

--- App/View/header.php
+++ App/View/header.php
@@ -11,11 +11,12 @@
         </div>
 
         <ul class="navbar">
-            <li><a href="/homepage#banner" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
-            <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
-            <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
-            <a href="connexion.php" ><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a>
+            <li><a class="element" href="/homepage#banner"><img class="images" src="/Web/images/home.png">Accueil</a><li>
+            <li><a href="/homepage#three"><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
+            <li><a href="/Web/index.php"><img class="images" src="/Web/images/contact.png">Contact</a><li>
+            <li><a href="connexion.php"><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a></li>
         </ul>
+
 
         <div class="burger-button">
           <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
--- Web/js/burger.js
+++ Web/js/burger.js
@@ -3,3 +3,9 @@ $('.burger-button').click(function(e) {
   $('.burger-button').toggleClass('active');
   $('.navbar').toggleClass('show');
 })
+
+// Remove header when clicking on a element of burger
+$('.navbar a').click(function(e) {
+  $('.burger-button').toggleClass('active');
+  $('.navbar').toggleClass('show');
+})
