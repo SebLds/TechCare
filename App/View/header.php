bd7c5c5b2dae593a26612543807639efc6b50730
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-06 15:59:05 +0200

    header responsive

--- App/View/header.php
+++ App/View/header.php
@@ -11,11 +11,12 @@
         </div>
 
         <ul class="navbar">
-            <li><a href="/homepage#banner" ><img class="images" src="/Web/images/home.png">Accueil</a><li>
-            <li><a href="/homepage#three" ><img class="images" src="/Web/images/test.png">Nos Tests</a><li>
-            <li><a href="/Web/index.php" ><img class="images" src="/Web/images/contact.png">Contact</a><li>
-            <a href="connexion.php" ><button id="button-connexion"><img class="images login" src="/Web/images/account.png">Connexion</button></a>
+            <li><a class="home" href="/homepage#banner"><i class="fad fa-home"></i>Accueil</i></a><li>
+            <li><a class="test" href="/homepage#three"><i class="fad fa-file-alt"></i>Nos Tests</a><li>
+            <li><a class="contact" href="/Web/index.php"><i class="fad fa-envelope-open"></i>Contact</a><li>
+            <li><a class="login" href="connexion.php"><button id="button-connexion"><i class="fad fa-user"></i>Connexion</button></a></li>
         </ul>
+
 
         <div class="burger-button">
           <!--<button id="burger-button"><img id="img-burger"src="images/burger.png"></button>-->
