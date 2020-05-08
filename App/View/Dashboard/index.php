a43b339ff398a5fbc21dd8f53c7aec5ee37dfb66
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-08 14:53:40 +0200

    dashboard

--- App/View/Dashboard/index.php
+++ App/View/Dashboard/index.php
@@ -7,25 +7,35 @@ $Score= [0,0] ;
 <link href="/Web/css/dashboard.css" rel="stylesheet" type="text/css">
 
 <div id="body">
-    <div class="welcomeMessage">
-        <div> <p>Mes derniers tests</p> </div>
-        <div> <p> x tests disponibles </p> </div>
+    <div class="title">
+        <div><p>Mes derniers tests</p></div>
+        <div><p> x tests disponibles</p></div>
         <button>Demande de test ?</button> <!-- Renvoyer l'utilisateur vers la page de contact -->
     </div>
 
     <div class="testResume">
         <div class="testResumeID">
-            <div><p><bold>Score : 0 > </bold> </p> </div>
+            <div><p>Score : 0</p></div>
             <div><p>Type de score</p></div>
             <button>Commentaire médecin</button>
         </div>
 
         <div class="testResumeID">
-            <div> <p> <bold>Score : 0 > </bold> </p> </div>
-            <div> <p> Type de score </p> </div>
-            <button> Commentaire médecin </button>
+            <div><p>Score : 0</p></div>
+            <div><p>Type de score</p></div>
+            <button>Commentaire médecin</button>
         </div>
 
-    </div>
+        <div class="testResumeID">
+            <div><p>Score : 0</p></div>
+            <div><p>Type de score</p></div>
+            <button>Commentaire médecin</button>
+        </div>
 
+        <div class="testResumeID">
+            <div><p>Score : 0</p></div>
+            <div><p>Type de score</p></div>
+            <button>Commentaire médecin</button>
+        </div>
+    </div>
 </div>
