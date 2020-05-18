bfd228e8a8a1ae5e8dda20ad7201c1f1771aa1a6
M-Schuch <58336186+M-Schuch@users.noreply.github.com>
2020-05-18 13:01:52 +0200

    Schuch

--- App/Model/Register.php
+++ App/Model/Register.php
@@ -7,12 +7,6 @@ use src\Model;
 
 class Register extends Model {
 
-    public function checkConnexion($mail, $password) {
-        try {
-            return $this -> executeRequest('SELECT idUtilisateur, "Type" FROM utilisateur WHERE Email = ? AND Motdepasse = ?', $mail, $password) -> fetch(PDO::FETCH_OBJ);
-        } catch (ConfigException $e) { return false; /*Message d'erreur*/ }
-    } //function checkConnexion($mail, $password)
-
     public function addNewUser($data) {
       $sqlStatement = 'INSERT INTO users (firstName, lastName, mail, password, birthdate, doctor, healthNumber) VALUES (:firstName, :lastName, :mail, :password, :birthdate, :doctor, :healthNumber)';
       try {
