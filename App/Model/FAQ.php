9f40f666a1f3a09807d41a91286c49ddbc387451
Pierre Tardif <pierre-tardif@orange.fr>
2020-05-28 16:56:17 +0200

    Revert "database"
    This reverts commit 73ac1be1

--- App/Model/FAQ.php
+++ App/Model/FAQ.php
@@ -9,7 +9,7 @@ use src\Model;
 class Faq extends Model {
 
   public function getFAQ() {
-    $sqlStatement = 'SELECT * FROM r4bgjpcm5dhsnnye.faq';
+    $sqlStatement = 'SELECT * FROM faq';
     try {
       return $this->executeRequest($sqlStatement)->fetchAll(PDO::FETCH_OBJ);
     } catch (ConfigException $e) {
@@ -17,7 +17,7 @@ class Faq extends Model {
   }
 
   public function addQuestion($question, $answer) {
-    $sqlStatement = 'INSERT INTO r4bgjpcm5dhsnnye.faq (question, answer, editDate) VALUES (:question, :answer, :editDate)';
+    $sqlStatement = 'INSERT INTO faq (question, answer, editDate) VALUES (:question, :answer, :editDate)';
     try {
       $date = Model::getDate();
       return $this->executeRequest($sqlStatement, array('question' => $question, 'answer' => $answer, 'editDate' => $date));
@@ -26,7 +26,7 @@ class Faq extends Model {
   }
 
   public function deleteQuestion($question) {
-    $sqlStatement = 'DELETE FROM r4bgjpcm5dhsnnye.faq WHERE question = :question';
+    $sqlStatement = 'DELETE FROM faq WHERE question = :question';
     try {
       return $this->executeRequest($sqlStatement, array('question' => $question));
     } catch (ConfigException $e) {
@@ -34,7 +34,7 @@ class Faq extends Model {
   }
 
   public function modifyQuestion($question, $answer) {
-    $sqlStatement = 'UPDATE r4bgjpcm5dhsnnye.faq SET question = :question, answer = :answer WHERE question = :question OR answer = :answer';
+    $sqlStatement = 'UPDATE faq SET question = :question, answer = :answer WHERE question = :question OR answer = :answer';
     try {
       return $this->executeRequest($sqlStatement, array('question' => $question, 'answer' => $answer));
     } catch (ConfigException $e) {
