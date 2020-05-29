b0ccca7bf708063e72eb72821072c9711a4e58ec
M-Schuch <58336186+M-Schuch@users.noreply.github.com>
2020-05-29 23:44:29 +0200

    Error

--- App/View/Error/index.php
+++ App/View/Error/index.php
@@ -1,11 +1,11 @@
-<?php $this->title='Erreur 404';?>
+<?php $this->title='Erreur';?>
 
 <link href="/Web/css/error.css" rel="stylesheet">
 
 <div id="body">
-    <div> <a href="/homepage"><img class="logo-error" src="/Web/images/logo.png"></a></div>
+    <div><a href="/homepage"><img class="logo-error" src="/Web/images/logo.png"></a></div>
     <div>
-    	<div class="message"><?php echo 'Erreur 404';?></div>
+    	<div class="message">Erreur 404</div>
         <div>Oups, cette page n'existe pas</div>
     </div>
 </div>
