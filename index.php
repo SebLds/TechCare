8e389845a5a7b9e5e6c4a07f7143a2474b117f1c
Pierre Tardif <pierre-tardif@orange.fr>
2020-06-02 13:13:17 +0200

    cgu

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $router->get('/homepage',"Home#index");
 $router->get('/homepage/:slug',"Home#index");
 
 $router->get('/faq',"Faq#index");
-$router->get('/cgu',"Cgu#index");
+$router->get('/cgu',"CGU#index");
 $router->get('/error-:id',"Error#generateError#id");
 $router->get('/contact',"Contact#index");
 
