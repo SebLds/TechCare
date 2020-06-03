9b8f1baa544e37b1bb39eaeac5f0de067b02af66
LouLou-ISEP <louis.felli@isep.fr>
2020-06-03 15:21:46 +0200

    é

--- App/View/Admin/index.php
+++ App/View/Admin/index.php
@@ -9,7 +9,7 @@
 			<h1 class="title">Utilisateurs</h1>
 		<div class="action">
 			<a href="/admin/add-user"><button class="btn add" type="submit" name="button"><i class="far fa-user-plus"></i>Ajouter</button></a>
-			<form class="search-bar" method="post">
+			<form class="search-bar" method="post" action="/admin/dashboard/result">
           <button class="sub-none" type="submit"><i class="fas fa-search fa-2x icon"></i></button>
           <input type="text" autocomplete="off" class="search-input" name='search' placeholder="Rechercher des utilisateurs...">
       </form>
