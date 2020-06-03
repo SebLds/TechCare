eaa1d3380e50ba7652def78716e8bc11f81def82
Pierre Tardif <pierre-tardif@orange.fr>
2020-06-03 22:13:07 +0200

    dashboard

--- App/View/Admin/index.php
+++ App/View/Admin/index.php
@@ -6,40 +6,56 @@
 <div id="body">
 
 	<div class="users">
-			<h1 class="title">Utilisateurs</h1>
+        <div class="title">
+            <h1>Utilisateurs</h1>
+        </div>
 		<div class="action">
-			<a href="/admin/add-user"><button class="btn add" type="submit" name="button"><i class="far fa-user-plus"></i>Ajouter</button></a>
 			<form class="search-bar" method="post" action="/admin/dashboard/result">
-          <button class="sub-none" type="submit"><i class="fas fa-search fa-2x icon"></i></button>
-          <input type="text" autocomplete="off" class="search-input" name='search' placeholder="Rechercher des utilisateurs...">
-      </form>
+                <a href="/admin/add-user"><button class="btn add" type="submit" name="button"><i class="fal fa-user-plus"></i>Ajouter</button></a>
+                <button class="sub-none" type="submit"><i class="fal fa-search fa-2x icon"></i></button>
+                <input type="text" autocomplete="off" class="search-input" name='search' placeholder="Rechercher des utilisateurs...">
+            </form>
 		</div>
 	</div>
 
 	<div class="users">
-			<h1 class="title">Foire aux questions</h1>
+        <div class="title">
+            <h1>Foire aux questions</h1>
+        </div>
 		<div class="action">
-			<a href="/admin/edit-faq"><button class="btn edit" type="submit" name="button"><i class="far fa-edit"></i>Modifier</button></a>
 			<form class="search-bar" method="post">
-          <button class="sub-none" type="submit"><i class="fas fa-search fa-2x icon"></i></button>
-          <input type="text" autocomplete="off" class="search-input" placeholder="Rechercher des utilisateurs...">
-      </form>
+                <a href="/admin/edit-faq"><button class="btn edit" type="submit" name="button"><i class="fal fa-edit"></i>Modifier</button></a>
+                <button class="sub-none" type="submit"><i class="fal fa-search fa-2x icon"></i></button>
+                <input type="text" autocomplete="off" class="search-input" placeholder="Rechercher des utilisateurs...">
+            </form>
 		</div>
 	</div>
 
 	<div class="users">
-			<h1 class="title">Forum</h1>
+        <div class="title">
+            <h1>Forum</h1>
+        </div>
 		<div class="action">
-			<a href="/forum/add-tag"><button class="btn add" type="submit" name="button">Ajouter une catégorie</button></a>
-			<a href="/forum/add-thread"><button class="btn add" type="submit" name="button">Ajouter un sujet</button></a>
+            <form>
+			    <a href="/forum/add-tag"><button class="btn add" type="submit" name="button"><i class="fal fa-folder-plus"></i>Ajouter une catégorie</button></a>
+            </form>
+            <form>
+			    <a href="/forum/add-thread"><button class="btn add" type="submit" name="button"><i class="fal fa-tags"></i>Ajouter un sujet</button></a>
+            </form>
 		</div>
 	</div>
 
 	<div class="users">
-			<h1 class="title">Test</h1>
+        <div class="title">
+            <h1>Test</h1>
+        </div>
 		<div class="action">
-			<a href="/admin/edit-faq"><button class="btn add" type="submit" name="button">Ajouter une type de test</button></a>
-			<a href="/admin/edit-faq"><button class="btn add" type="submit" name="button">Ajouter un sujet</button></a>
+            <form>
+                <a href="/admin/edit-faq"><button class="btn add" type="submit" name="button"><i class="fal fa-tachometer-alt-fastest"></i>Ajouter une type de test</button></a>
+            </form>
+            <form>
+                <a href="/admin/edit-faq"><button class="btn add" type="submit" name="button"><i class="fal fa-tags"></i>Ajouter un sujet</button></a>
+            </form>
 		</div>
 	</div>
 
