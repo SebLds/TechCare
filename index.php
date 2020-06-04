09c96bb1102914211e7046aca9f8cdb3aba39038
M-Schuch <58336186+M-Schuch@users.noreply.github.com>
2020-06-04 13:50:38 +0200

    CGU

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $router->get('/homepage',"Home#index");
 $router->get('/homepage/:slug',"Home#index");
 
 $router->get('/faq',"Faq#index");
-$router->get('/cgu',"CGU#index");
+$router->get('/cgu',"Cgu#index");
 $router->get('/error-:id',"Error#generateError#id");
 $router->get('/contact',"Contact#index");
 
