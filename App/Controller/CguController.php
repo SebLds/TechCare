2535056c08bf941d06d84d54f4e2c6b92bad021c
M-Schuch <58336186+M-Schuch@users.noreply.github.com>
2020-06-04 13:58:52 +0200

    Update CguController.php

--- App/Controller/CguController.php
+++ App/Controller/CguController.php
@@ -5,12 +5,8 @@ namespace App\Controller;
 use src\Controller;
 
 
-class CguController extends Controller
-{
-    public function index()
-    {
-
+class CguController extends Controller {
+    public function index() {
         $this->generateView(array(),'index');
     }
-
 }
